7db51b1ac470ef46f543b11104263fcfa8367bd2
Sahil Shetty <sahilshetty58@gmail.com>
2024-01-18 00:33:03 -0500

    WDDM 115 Assignment 1

--- wddm 113 & 114/Week 2/script.js
+++
@@ -1,5 +0,0 @@
-let count = document.getElementById("count").innerText;
-document.getElementById("add").addEventListener("mouseover", () => {
-  count++;
-  document.getElementById("count").innerText = count;
-});
